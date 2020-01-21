a13c976fd9df046234d033add392e18e926bd76d
Luck-Jeh <jeheeluqman@gmail.com>
2020-01-21 18:47:30 -0500

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,9 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +23,45 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13 + 1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        Card c = new Card();
+        Scanner scanner = new Scanner(System.in);
+        int value = 0;
+        String suit;
+        do{
+            System.out.print("Input a card value :");
+            value = scanner.nextInt();
+            if (value<1 || value >13 )
+                System.out.println("Invalid value. Please enter again.");
+        }while (value<1 || value >13 );
+        scanner.nextLine(); // Flush out the scanner buffer
+        while (true){
+            System.out.print("Input a card suit :");
+            suit = scanner.nextLine();
+            if (suit.equals("Spades") || suit.equals("Diamonds") || suit.equals("Hearts") || suit.equals("Clubs"))
+                break;
+            else
+                System.out.println("Invalid suit. Please enter again.");
+        }
+        c.setValue(value);
+        c.setSuit(suit);
+        
+        
+        boolean flag = false;
+        for (int x=0; x<magicHand.length; x++)
+        {
+            if ((c.getSuit().equals(magicHand[x].getSuit())) && (c.getValue() == magicHand[x].getValue()))
+                flag = true;
+        }
+        
+        if (flag)
+            System.out.println("Card was in the magic hand");
+        else
+            System.out.println("Card was not in the magic hand");
     }
     
 }
